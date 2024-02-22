387d5aa6d9a8aff795ce7ba333de4e6d3481c44e
vrundadhamsania <155124170+vrundadhamsania@users.noreply.github.com>
2024-02-22 08:54:28 +0530

    Update b1.php

--- b1.php
+++ b1.php
@@ -18,7 +18,7 @@ else
 	echo "the file does not exists";
 }
 $myfile=fopen("b1_enrollment.txt","a");
-$new_contect="append contect GAJER APRINSI GSDYGE GYESG ";
+$new_contect="append contect vrunda dhamsania GSDYGE GYESG ";
 file_put_contents("b1_enrollment.txt",$new_contect,FILE_APPEND);
 unlink("b1_enrollment.txt");
 
